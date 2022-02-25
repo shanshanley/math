59e07eba6da960523979e2fcba9329ebb8920bbc
shanshanley <98719942+shanshanley@users.noreply.github.com>
2022-02-26 02:19:44 +0800

    Update and rename README.md to index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<body bgcolor="#6BA961">
+<td width="33.33%" valign="top">
+<table border="0" width="100%" cellpadding="15" cellspacing="0" align="center" bgcolor="#353535">
+<tr> <td> 
+<font face="arial" size="5" color="#ffffff">
+<center> Performance Task in Mathematics 9 </font> 
+<br> <br> 
+<a href="https://youtu.be/G7s5DTj118E"> <font face="arial" size="4" color="#03EE5C"> Clink here to access the Video Presentation
+<br> <br>
+<a href="https://docs.google.com/document/d/1ydIQquctU8sLwpUgsP9rwjbkbHr_R0BKxndefp5dfr4/edit?usp=sharing"> <font face="arial" size="4" color="#03EE5C"> Clink here to access the Drawing
+</center> </font> <br> <br>
+</td> </tr> </table> </td> </html>
